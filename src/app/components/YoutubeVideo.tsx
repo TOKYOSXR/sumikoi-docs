7c268f9aac427775c3ea0461c8285b0c1e2cfb91
henriqueworks <henriqueworks68@gmail.com>
2025-08-01 21:23:24 -0300

    comentando codigo do componente YoutubeVideo

--- src/app/components/YoutubeVideo.tsx
+++ src/app/components/YoutubeVideo.tsx
@@ -1,6 +1,9 @@
+// Componente para incorporar um vídeo do YouTube nas páginas
+// Recebe uma propriedade videoId, que é o identificador do vídeo do YouTube a ser exibido
+
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 
-export default function YoutubeVideo ({ videoId}: { videoId: string }){
+export default function YoutubeVideo ({ videoId }: { videoId: string }) {
     return (
         <Card className="max-w-3xl mx-auto">
             <CardHeader>
@@ -13,7 +16,7 @@ export default function YoutubeVideo ({ videoId}: { videoId: string }){
                     src={`https://www.youtube.com/embed/${videoId}`}
                     title="Tutorial"
                     className="w-full h-full rounded-md"
-                    frameBorder= "0"
+                    frameBorder="0"
                     allowFullScreen
                 />
             </CardContent>
