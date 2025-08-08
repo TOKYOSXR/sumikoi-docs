47b1959478c1c11e8f99ff616ca690ac36201190
henriqueworks <henriqueworks68@gmail.com>
2025-08-08 20:20:57 -0300

    style: estilo dark mode escuro alterado

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -3,7 +3,7 @@
 
 export default function Footer() {
     return (
-        <footer className="border-t border-[#0F0F10]/20 h-20 p-4 flex items-center justify-center bg-white text-black dark:bg-black dark:text-[#d81e1e] dark:border-[#d81e1e]">
+        <footer className="border-t border-[#0F0F10]/20 h-20 p-4 flex items-center justify-center bg-white text-black dark:bg-[#171717] dark:text-[#d81e1e] dark:border-[#d81e1e]">
             <p>© 2025 Sumikoi Documentation. Todos os direitos reservados.</p>
         </footer>
     );
